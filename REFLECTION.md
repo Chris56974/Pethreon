6c622f3b56fb9081a38f78ed6fea09468bfb9a6b
chris <skylake112@outlook.com>
2021-03-03 14:55:25 -0700

    Add some config and other setup

---
+++ REFLECTION.md
@@ -0,0 +1,5 @@
+# Questions I had when developing the project
+
+1. Multiple testing frameworks?
+
+I like Jest but it looks like Truffle uses Mocha and Chai. Since I only want one testing framework I removed Jest.
--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,6 @@
 /**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * trufflesuite.com/docs/advanced/configuration
- *
  * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
+ * to sign your transactions before they're sent to a remote public node. 
  *
  * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
  * public/private key pairs. If you're publishing your code to GitHub make sure you load this
@@ -54,7 +44,7 @@ module.exports = {
     // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
     // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
     // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websocket: true        // Enable EventEmitter interface for web3 (default: false)
+    // websocket: true         // Enable EventEmitter interface for web3 (default: false)
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
