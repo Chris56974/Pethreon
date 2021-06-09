0c569931bcfdbf7e2481ea8c20fad77903122771
chris <skylake112@outlook.com>
2021-06-08 18:05:59 -0600

    Layout looks good here

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
         <h1 className="pethreon">P<span className="Ξ">Ξ</span>threon</h1>
         <ul className="features">
           <li>Contribute monthly to your favourite creators in a trustless, and privacy respecting manner</li>
-          <li>Only pay transaction fees, <a href="https://github.com/Chris56974/Pethreon/blob/main/contracts/Pethreon.sol">view the smart contract here on Github <Github /></a></li>
+          <li>Only pay transaction fees, <a href="https://github.com/Chris56974/Pethreon/blob/main/contracts/Pethreon.sol">view the smart contract on Github</a> <Github /></li>
         </ul>
         <p className="metamessage">You need to <a href="https://metamask.io/download">download metamask</a> for this application to work!</p>
         <div className="metamask">
