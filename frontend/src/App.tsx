adcb0ba765f451ac7983f1036c0ef0317fa01644
chris <skylake112@outlook.com>
2021-06-22 19:02:35 -0600

    refactor circles into their own component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,13 @@
 import { Login } from './pages/login/login';
 import { Contribute } from './pages/contribute/contribute';
 import { Create } from './pages/create/create';
-import { Switch, Route, Redirect, useLocation, useHistory } from 'react-router-dom';
-import "./pages/contribute/circle.css";
-import "./pages/create/circle.css";
-
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { Circles } from './components/circles/circles';
 
 const App: React.FC = () => {
-  const history = useHistory()
-  const location = useLocation()
-  const loginPage = location.pathname === '/'
-  const contributePage = location.pathname === '/contribute'
-  const createPage = location.pathname === '/create'
-
-  const switchPortals = () => {
-    if (contributePage) history.push("/create")
-    if (createPage) history.push("/contribute")
-  }
-
-  // Circle animation depends on the route
-  const circleAnimation = (page: string) => {
-    if (createPage) return `circle${page}_create`
-    if (contributePage) return `circle${page}_contribute`
-    return `circle${page}`
-  }
-
   return (
     <>
-      <div className={circleAnimation("A")} />
-      <button className={circleAnimation("B")} onClick={switchPortals} disabled={loginPage ? true : false} >
-        {contributePage ? "Switch to Creator Portal" : ""}
-        {createPage ? "Switch to Contributor Portal" : ""}
-      </button>
-      <div className={circleAnimation("C")} />
+      <Circles />
       <Switch>
         <Route path="/" exact component={Login} />
         <Route path="/contribute" exact component={Contribute} />
---
+++ frontend/src/components/circles/circles.tsx
@@ -0,0 +1,29 @@
+import { useLocation, useHistory } from "react-router"
+
+export const Circles = () => {
+  const history = useHistory()
+  const location = useLocation()
+  const loginPage = location.pathname === '/'
+  const contributePage = location.pathname === '/contribute'
+  const createPage = location.pathname === '/create'
+
+  const switchPortals = () => {
+    if (contributePage) history.push("/create")
+    if (createPage) history.push("/contribute")
+  }
+
+  // Circle animation depends on the route
+  const circleAnimation = (page: string) => {
+    if (createPage) return `circle${page}_create`
+    if (contributePage) return `circle${page}_contribute`
+    return `circle${page}`
+  }
+  return <>
+    <div className={circleAnimation("A")} />
+    <button className={circleAnimation("B")} onClick={switchPortals} disabled={loginPage ? true : false} >
+      {contributePage ? "Switch to Creator Portal" : ""}
+      {createPage ? "Switch to Contributor Portal" : ""}
+    </button>
+    <div className={circleAnimation("C")} />
+  </>
+}
