bd095d642e96231c40d248e98cb201ba61b96899
Chris S <skylake112@outlook.com>
2021-11-29 11:40:09 -0700

    switch to yarn workspaces

--- frontend/src/pages/login/components/Pethreon/Pethreon.tsx
+++
@@ -1,7 +0,0 @@
-interface PethreonProps {
-  className: string
-}
-
-export const Pethreon = ({ className }: PethreonProps) => (
-  <h1 className={className}>P<span>Ξ</span>threon</h1>
-)
--- frontend/src/pages/login/components/index.ts
+++
@@ -1,7 +0,0 @@
-import { Features } from "./Features/Features";
-import { LoginContainer } from "./LoginContainer/LoginContainer";
-import { Pethreon } from "./Pethreon/Pethreon";
-import { TypewriterEffect } from "./TypewriterEffect/TypewriterEffect";
-import { Video } from "./Video/Video";
-
-export { Features, LoginContainer, Pethreon, TypewriterEffect, Video }
