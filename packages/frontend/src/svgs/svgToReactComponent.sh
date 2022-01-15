7a590412303ce8dce9eef8efc464cc2547eb625d
Chris S <skylake112@outlook.com>
2022-01-15 13:15:48 -0700

    refactor the circles, action buttons, and modals

--- packages/frontend/src/svgs/svgToReactComponent.sh
+++ packages/frontend/src/svgs/svgToReactComponent.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
+# Feed this script a file that uses kebab case React attributes 
+# and it'll mutate those attributes in place to their camelCase equivalents
+
 if [ ! -f $1 ]; then
   echo "not a file"
   echo $1
   exit 1
 fi
 
-# Non-exhaustive, I'll add more as I go on
-
+# This list is non-exhaustive, I'll add more as I go on
+# -i is "inplace" meaning edit the file in place instead of writing to stdout
 sed -i "
   s/stroke-miterlimit/strokeMiterlimit/g;
   s/stroke-width/strokeWidth/g
