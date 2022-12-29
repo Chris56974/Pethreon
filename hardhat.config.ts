5eb8e45c5df6dc861fc3d2e3bfd1de972747a1fa
Chris S. WSL <skylake112@outlook.com>
2022-12-28 18:13:52 -0700

    add dotenv

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,12 @@
+import * as dotenv from "dotenv"
+dotenv.config()
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+/** 
+ * I have a .env file in this directory
+ */
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.17",
@@ -8,12 +14,12 @@ const config: HardhatUserConfig = {
   typechain: {
     outDir: "frontend/typechain-types"
   },
-  // networks: {
-  //   sepolia: {
-  //     url: `${process.env.INFURA_SEPOLIA_URL}`,
-  //     accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`],
-  //   }
-  // }
+  networks: {
+    sepolia: {
+      url: `${process.env.INFURA_SEPOLIA_URL}`,
+      accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`],
+    }
+  }
 };
 
 export default config;
